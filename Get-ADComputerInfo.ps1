43981ebc5039794a9374d47b1544e4243151aff2
msteed-null-byte <msteed@null-byte.xyz>
2024-05-20 14:11:56 -0400

    Added Credit Block

--- Get-ADComputerInfo.ps1
+++ Get-ADComputerInfo.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+    Retrieves information about systems from Active Directory and exports it to a CSV file.
+
+.DESCRIPTION
+    This script reads a list of system names from a text file, retrieves information about each system
+    from Active Directory, and exports the collected information to a CSV file.
+
+.NOTES
+    Author: Michael Steed
+    Date: 2020-01-07
+    Version: 1.0
+    Last Updated: 2024-05-20
+
+    This script provides a convenient way to collect system information from Active Directory and store it in a CSV file.
+
+.PARAMETER systemListFile
+    Specifies the path to the text file containing the list of system names.
+
+.PARAMETER outputFile
+    Specifies the path to the output CSV file where the collected information will be stored.
+
+.LINK
+    https://example.com/documentation
+#>
+
+
 # Input and output file paths
 $systemListFile = "C:\path\to\system_list.txt"
 $outputFile = "C:\path\to\output.csv"
